391f27bec5d3ab0e91d7e3ab11f271d967e886e6
avedeveloper <developer@soyave.com>
2023-03-31 11:54:05 -0300

    cors 4

--- backend/admin/src/main.ts
+++ backend/admin/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'],
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
